95844e6995d2c9ffd56510ef578a6141860a7617
Chris Wang <heyitschrisw@gmail.com>
2021-11-29 12:12:59 -0800

    rendered canvas to display draggable rectangle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import { Stage, Layer } from 'react-konva';
+import Canvas from './Canvas'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    console.log('success')
+    setNumPages(numPages);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
+
+      <Document
+        file="/sample/sample.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={console.error}
+      >
+          <Page pageNumber={pageNumber} scale={2.0} />
+      </Document>
+      <Canvas style={{position: "relative", top: "0px", left: "0px", zIndex: 2}} />
+
+      <p>Page {pageNumber} of {numPages}</p>
     </div>
   );
 }
